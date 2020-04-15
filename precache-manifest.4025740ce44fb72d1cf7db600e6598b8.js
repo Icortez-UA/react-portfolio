20e45a758619950c06888700d69903d0af009865
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2020-04-15 13:44:53 -0700

    Updates

---
+++ precache-manifest.4025740ce44fb72d1cf7db600e6598b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f748cfa683e86dbdc10f906b08c92a37",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "7613bdea6d9d3e247d3b",
+    "url": "/react-portfolio/static/css/main.bdc4809d.chunk.css"
+  },
+  {
+    "revision": "7628373848f22d9a20c4",
+    "url": "/react-portfolio/static/js/2.b708cf52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.b708cf52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7613bdea6d9d3e247d3b",
+    "url": "/react-portfolio/static/js/main.caa0e6ea.chunk.js"
+  },
+  {
+    "revision": "0346f5e54be923396aed",
+    "url": "/react-portfolio/static/js/runtime-main.8a7d631b.js"
+  },
+  {
+    "revision": "017f0611a510a5a8b5184e6dc0db65d7",
+    "url": "/react-portfolio/static/media/biophoto.017f0611.jpg"
+  },
+  {
+    "revision": "a46e8467f4933cafd79d608cd49d2619",
+    "url": "/react-portfolio/static/media/janelogo.a46e8467.jpg"
+  },
+  {
+    "revision": "ef8f7391f1d1710083b145b55e0d7db9",
+    "url": "/react-portfolio/static/media/league-finder.ef8f7391.jpg"
+  }
+]);
